9ddd7f99d9eb924ed6b5b0251b11180daaa66b0f
Taha Mir <t.miranzadeh@outlook.com>
2022-04-30 14:58:45 +0200

    :)

--- Hop-Project.py
+++ Hop-Project.py
@@ -4,5 +4,11 @@ Created on Sat Apr 30 14:41:45 2022
 
 @author: tahat
 """
-
+import matplotlib.pyplot as plt
+import numpy as np
 from neurodynex3.hopfield_network import pattern_tools, plot_tools, network
+
+pattern_size = 300
+
+# create an instance of the class HopfieldNetwork
+hopfield_net = network.HopfieldNetwork(nr_neurons= pattern_size**2)

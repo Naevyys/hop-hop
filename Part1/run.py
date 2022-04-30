47ff5ad7133d22a506c28de515650ecc8e61a138
Naevyys <siriremerk@gmail.com>
2022-04-30 20:11:37 +0200

    Exercise 5 done

--- Part1/run.py
+++ Part1/run.py
@@ -93,10 +93,10 @@ def ex3(net, stored_patterns, distance_function, selected_pattern_id=None):
     return distances, correctly_retrieved, selected_pattern_id
 
 
-def ex4(net, distance_function, m_vals=(5, 20, 40, 60, 80, 100), n_runs=6):
+def ex4(net, distance_function, m_vals=(5, 20, 40, 60, 80, 100), n_runs=6, plot=True):
     """
     Computes for each value of m the mean distance between the final state of the network and the target pattern, and
-    plot the results.
+    plot the results if plot == True.
 
     Algorithm:
     - For 5 to 8 values M from 5 to 100:
@@ -112,6 +112,7 @@ def ex4(net, distance_function, m_vals=(5, 20, 40, 60, 80, 100), n_runs=6):
     :param distance_function: distance function to use
     :param m_vals: List of different values for m
     :param n_runs: Number of runs to reach the state to compare with the target pattern
+    :param plot: Whether to plot and save the plot or not
     :return: mean distances
     """
 
@@ -128,18 +129,40 @@ def ex4(net, distance_function, m_vals=(5, 20, 40, 60, 80, 100), n_runs=6):
         return np.mean(distances)
 
     means = np.array([compute_mean_distance(m) for m in m_vals])
-    plt.plot(m_vals, means)
-    plt.title("Ex4: Means distances between final state and target pattern.")
-    plt.xlabel("Number of patterns stored in the network")
-    plt.ylabel("Error (measured using Hamming distance)")
-    plt.savefig("plots/ex4.png")
-    plt.show()
+
+    if plot:
+        plt.plot(m_vals, means)
+        plt.title("Ex4: Means distances between final state and target pattern.")
+        plt.xlabel("Number of patterns stored in the network")
+        plt.ylabel("Error (measured using Hamming distance)")
+        plt.savefig("plots/ex4.png")
+        plt.show()
 
     return means
 
 
-def ex5():
-    raise NotImplementedError
+def ex5(net, distance_function, n_trials=8):
+    """
+    Repeats experiment of ex4 several times to obtain error bars on the means, and plots the results.
+    :param net: hopfield network
+    :param distance_function: function used to compute the distance
+    :param n_trials: number of times the experiment should be repeated to get the error bars.
+    :return: mean of means, std of means
+    """
+
+    m_vals = (5, 20, 40, 60, 80, 100)
+    means = np.array([ex4(net, distance_function, m_vals=m_vals, plot=False) for _ in range(n_trials)])
+    mean_of_means = np.mean(means, axis=0)
+    std_of_means = np.std(means, axis=0)  # TODO: change if needed according to TA answers
+
+    plt.errorbar(m_vals, mean_of_means, yerr=std_of_means)
+    plt.title("Ex5: Means distances with errorbars between final state and target pattern.")
+    plt.xlabel("Number of patterns stored in the network")
+    plt.ylabel("Error (measured using Hamming distance)")
+    plt.savefig("plots/ex5.png")
+    plt.show()
+
+    return mean_of_means, std_of_means
 
 
 if __name__ == "__main__":
@@ -147,4 +170,11 @@ if __name__ == "__main__":
     compute_hamming_distance = ex2()  # TODO: answer theory question of ex2
     distances, correctly_retrieved, selected_pattern_id = ex3(net, stored_patterns, compute_hamming_distance)
     means = ex4(net, compute_hamming_distance)  # TODO: answer theory question of ex4
+    mean_of_means, std_of_means = ex5(net, compute_hamming_distance)
+
+
+# Questions for the TAs:
+# - Ex4: "what is the maximum number of patterns that can be retrieved?" What kind of answer is expected from us there?
+#   A theory answer or a computational answer?
+# - Ex5: Which error bars are you expecting? min/max? std? 95% confidence interval?
 
